b3561fa1e287f9ddade9bca0816982be183e43f7
jOS150702 <112134335+jOS150702@users.noreply.github.com>
2022-08-25 18:43:50 -0500

    Add files via upload

---
+++ JavaScript1.js
@@ -0,0 +1,16 @@
+﻿const yesbtn = document.querySelector('#yesbtn');
+
+yesbtn.addEventListener('click', function () {
+    alert('Sabia que no te ibas a negar  shiquito 7u7r');
+
+});
+
+const nobtn = document.querySelector('#nobtn');
+
+nobtn.addEventListener('mouseover', function () {
+    const randomx = parseInt(Math.random() * 90);
+    const randomy = parseInt(Math.random() * 100);
+    nobtn.style.setProperty('top', randomy + '%');
+    nobtn.style.setProperty('left', randomx + '%');
+    nobtn.style.setProperty('transform', `translate(-${randomX}%,-${randomY}%)`);
+})
